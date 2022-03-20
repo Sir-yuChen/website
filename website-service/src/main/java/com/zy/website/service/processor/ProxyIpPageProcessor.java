5dbeac8918a1061a657c29cdf167e10f686d0e85
zhangyu <2470838639@qq.com>
2022-03-20 22:34:50 +0800

    2022.03.20 java 爬虫 IP代理池

--- website-service/src/main/java/com/zy/website/service/processor/ProxyIpPageProcessor.java
+++ website-service/src/main/java/com/zy/website/service/processor/ProxyIpPageProcessor.java
@@ -1,33 +1,92 @@
 package com.zy.website.service.processor;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *  用于解析爬取到的XXX html页面
  **/
 public class ProxyIpPageProcessor implements PageProcessor {
 
+    private static Logger logger = LogManager.getLogger(ProxyIpPageProcessor.class);
+
     // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
     // webmagic官方还有很多案例，更多内容自行参考学习了，例如配置代理，自带url去重、网页去重等功能
     // 官方文档地址：http://webmagic.io/docs/zh/
-    private String url = "https://proxy.mimvp.com/freeopen";
-
+    public static void main(String[] args) {
+        Spider.create(new ProxyIpPageProcessor())
+                //在这里写上自己博客地址，从这个地址开始抓
+                .addUrl("http://www.66ip.cn/")
+                .addPipeline(new ProxyIpPipeline())
+                .run();
+    }
+    private Integer total = 0;
+    private Integer requestCount = 0;
+    private List rultes;
     @Override
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
-        //用于获取所有满足这个正则表达式的链接
-        List<String> links = page.getHtml().links().regex("https://proxy.mimvp\\.com/freeopen?proxy=in_hp&sort=&page=52").all();
-
+        logger.info("获取代理IP池开始 爬虫{}",requestCount);
+        List<String> ruls = new ArrayList<>();
+        List<String> links =new ArrayList<>();
+        if (total == 0) {
+            String pagesPath_t="http://www\\.66ip\\.cn/[0-9]+\\.html";
+            List<Integer> integers = new ArrayList<>();
+            ruls.add(pagesPath_t);
+            for (int i = 0; i < ruls.size(); i++) {
+                //用于获取所有满足这个正则表达式的链接
+                List<String> all = page.getHtml().links().regex(ruls.get(i)).all().stream().distinct().collect(Collectors.toList());
+                all.forEach(path->{
+                    int index = path.lastIndexOf("/");
+                    String substring = path.substring(index + 1);
+                    int index1 = substring.lastIndexOf(".");
+                    String number = substring.substring(0, index1);
+                    integers.add(Integer.valueOf(number));
+                });
+            }
+            total = integers.stream().distinct().max(Integer::compare).get();
+            for (int j = 1; j <= 10; j++) {
+                links.add("http://www.66ip.cn/"+j+".html");
+            }
+            page.addTargetRequests(links);
+        }
+        //将这些链接加入到待抓取的队列中去
+        List<String> list = page.getHtml().xpath("//div[@class='layui-row layui-col-space15']//div[@align='center']//table//tbody//tr").all();
+        list.remove(0);
+        requestCount++;
+        String url1 = page.getRequest().getUrl();
+        logger.info("获取代理IP池开始 爬虫  请求URL{} 获取数据{}", url1,JSONObject.toJSONString(list));
+        if (rultes == null) {
+            rultes = list;
+        }else {
+            rultes.addAll(list);
+        }
+        page.setSkip(true);
+        if (requestCount > 1) {
+            int i = url1.lastIndexOf("/") + 1;
+            int i1 = url1.lastIndexOf(".");
+            String pageNum = url1.substring(i, i1);
+            if (Integer.valueOf(pageNum)==10) {
+                page.setSkip(false);
+                page.putField("rultes",rultes);
+                logger.info("获取代理IP池开始 爬虫 =={}", JSONObject.toJSONString(rultes));
+            }
+        }
     }
 
     @Override
     public Site getSite() {
-        return MySite.getSit(site);
+        return site;
     }
 }
--- website-service/src/main/java/com/zy/website/service/processor/ProxyIpPipeline.java
+++ website-service/src/main/java/com/zy/website/service/processor/ProxyIpPipeline.java
@@ -3,12 +3,17 @@ package com.zy.website.service.processor;
 import com.zy.website.mapper.ProxyIpMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 用于将XXX html页面解析出的数据存储到mysql数据库
@@ -23,9 +28,23 @@ public class ProxyIpPipeline implements Pipeline {
 
     @Override
     public void process(ResultItems resultItems, Task task) {
+        List<String> rultes = (List<String>) resultItems.get("rultes");
+        logger.info("ProxyIpPipeline {}", rultes );
+        //todo 数据量大使用strem 并行流 待痛优化
+        rultes.forEach(str->{
+            //jsoup 解析判断Html会有问题  https://www.cnblogs.com/zhangyinhua/p/8037599.html
+            String html="<html>  <body> <table> <tbody>"+str+"</tbody> </table> </body> </html>";
+            Document document = Jsoup.parseBodyFragment(html);
+            Element body = document.body();
+            Elements elementsByTag = body.getElementsByTag("td");
 
+            String ip = elementsByTag.get(0).text();
+            String port= elementsByTag.get(1).text();
+            String ad = elementsByTag.get(2).text();
+            logger.info("获取TD标签下的内容 ip={} port={} ad={}",ip,port,ad);
 
+            //TOdo 获取IP的信息 https://ip.taobao.com/service/getIpInfo.php?ip=27.192.200.105  https://ip.taobao.com/service/getIpInfo.php?ip=43.132.246.151
 
-
+        });
     }
 }
